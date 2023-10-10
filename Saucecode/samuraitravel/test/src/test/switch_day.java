b3a1e3cfb27a8cc23af86b255a4a48df59adb484
Yomogi13 <kannzaki.a.s.i@gmail.com>
2023-10-10 23:17:26 +0900

    update 10/10

---
+++ Saucecode/samuraitravel/test/src/test/switch_day.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.util.Scanner;
+
+public class switch_day {
+
+	public static void main(String[] args) {
+
+		switch (1) {
+		case 1:
+			System.out.println("月曜日");
+			break;
+		case 2:
+			System.out.println("火曜日");
+			break;
+		case 3:
+			System.out.println("水曜日");
+			break;
+		case 4:
+			System.out.println("木曜日");
+			break;
+		case 5:
+			System.out.println("金曜日");
+			break;
+		case 6:
+			System.out.println("土曜日");
+			break;
+		case 7:
+			System.out.println("日曜日");
+			break;
+		default:
+			System.out.println("無効な曜日");
+		}
+
+	}
+}
